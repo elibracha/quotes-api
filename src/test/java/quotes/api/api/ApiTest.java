83cb8e30ab541dda2174a1d20b0cd0a7bda2424c
Eli Bracha <elibracha45@gmail.com>
2019-08-25 01:43:59 +0300

    added unit testing

--- src/test/java/quotes/api/api/ApiTest.java
+++ src/test/java/quotes/api/api/ApiTest.java
@@ -1,38 +1,58 @@
 package quotes.api.api;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Unit test for simple App.
- */
-public class ApiTest 
-    extends TestCase
-    {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public ApiTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( ApiTest.class );
-    }
+import quotes.api.errors.QuoteError;
+import quotes.api.model.Quote;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class ApiTest extends AbstractTest {
+
+	@Override
+	@Before
+	public void setUp() {
+		super.setUp();
+	}
+
+	@Test
+	public void postQuoteWithEmptyName() throws Exception {
+		String uri = "/api/v1/quotes";
+		Quote quote = new Quote();
+		quote.setName("");
+	
+		String inputJson = super.mapToJson(quote);
+		MvcResult mvcResult = mvc.perform(
+				MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson))
+				.andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(400, status);
+		QuoteError error = super.mapFromJson(mvcResult.getResponse().getContentAsString(), QuoteError.class);
+		assertEquals(error.getDescription(), "name field can not be empty");
+	}
+	
+	@Test
+	public void postQuoteWithNegativePrice() throws Exception {
+		String uri = "/api/v1/quotes";
+		Quote quote = new Quote();
+		quote.setName("random");
+		quote.setPrice(-1L);
+		
+		String inputJson = super.mapToJson(quote);
+		MvcResult mvcResult = mvc.perform(
+				MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson))
+				.andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(400, status);
+		QuoteError error = super.mapFromJson(mvcResult.getResponse().getContentAsString(), QuoteError.class);
+		assertEquals(error.getDescription(), "price can not be a negative number");
+	}
+	
+	
 }
